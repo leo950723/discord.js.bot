3be1af1807948b32bf050928532ed2b3984d30f9
leo950723 <leo950723op@gmail.com>
2024-02-18 23:48:41 +0800

    [EP4] 初始化vue pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
 import dotenv from'dotenv'
+import vuelnit from'@/core/vue'
 
+vuelnit()
 dotenv.config()
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
