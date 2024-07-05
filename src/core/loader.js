e5f6401ae2004db6406f986229e6d3b6d75ac8e5
leo950723 <113075849+leo950723@users.noreply.github.com>
2024-07-06 03:20:14 +0800

    [EP5]斜線指令註冊

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,12 +1,12 @@
 import {REST,Routes}from "discord.js"
 
 
-const Registeringslashcommands =(guilid)=>{
+const Registeringslashcommands =()=>{
     const rest =new REST({version:10}).setToken(process.env.TOKEN)
     rest.put(
         Routes.applicationGuildCommands(
-            process.env.APPLICATION_ID,
-            guilid=process.env.GUILDID_ID,
+            process.env.APPLICATION_ID
+          
         )
         
     )
