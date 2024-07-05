0a1514ade7c409d9fa546bf9dd585c1e653765fd
leo950723 <leo950723op@gmail.com>
2024-07-06 02:14:13 +0800

    [EP5]讀取本地所有資料檔案

--- src/core/loader.js
+++ src/core/loader.js
@@ -1,3 +1,13 @@
-const Registeringslashcommands =()=>{
+import {REST,Routes}from "discord.js"
 
+
+const Registeringslashcommands =(guilid)=>{
+    const rest =new REST({version:10}).setToken(process.env.TOKEN)
+    rest.put(
+        Routes.applicationGuildCommands(
+            process.env.APPLICATION_ID,
+            guilid=process.env.GUILDID_ID,
+        )
+        
+    )
 }
